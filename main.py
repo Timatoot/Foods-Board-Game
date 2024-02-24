0a54c441f248af722513641b27f7bff117a44f93
Tim Rostorhuiev <tim4monkey@gmail.com>
2024-02-23 22:45:27 -0500

    added clues and clue display

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ current_roll = 1
 
 rolled = False
 
-at_target_square = True
+at_target_square = False
+
+can_move = False
 
 locations = {
     1: (113, 58),
@@ -37,11 +39,11 @@ locations = {
     20: (566, 619),
     21: (682, 619),
     22: (682, 722)
-} 
- 
+}
+
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Food Detective")
- 
+
 background_image = pygame.image.load("assets/Background.png")
 
 background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
@@ -53,10 +55,54 @@ def draw_circle(square_number):
 def roll_dice():
     return random.randint(1, 6)
 
+def pop_up_message(message):
+    font = pygame.font.Font(None, 36)
+    lines = message.split('\n')  # Split the message into lines
+    
+    # Calculate the starting y position so the block of text is centered
+    block_height = len(lines) * font.get_height()
+    y_start = screen_height // 2 - block_height // 2
+    
+    screen.fill((0, 0, 0))  # Fill the screen with black before displaying the message
+
+    for i, line in enumerate(lines):
+        text = font.render(line, True, (255, 255, 255))
+        text_rect = text.get_rect(center=(screen_width // 2, y_start + i * font.get_height()))
+        screen.blit(text, text_rect)
+    
+    pygame.display.flip()
+    # Event loop to wait for ESC key press or mouse button click
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:  # Close popup when ESC is pressed
+                    return
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                return  # Close popup on mouse click
+            
+def get_clue_from_file(file_name):
+    with open(file_name, "r") as file:
+        return file.read()
+
+def clear_text_file(file_name):
+    with open(file_name, "w") as file:
+        file.write("")
+
+def write_clue_to_file(file_name, clue):
+    with open(file_name, "a") as file:
+        file.write("\n\n" + clue)
+
+def add_new_clue(clue):
+    write_clue_to_file("known clues.txt", clue)
+
 def roll_button():
     global current_roll
     global rolled
     global at_target_square
+    global can_move
 
     button_x = 700
     button_y = 20
@@ -71,7 +117,8 @@ def roll_button():
     click = pygame.mouse.get_pressed()
 
     if button_x + button_width > mouse[0] > button_x and button_y + button_height > mouse[1] > button_y:
-        if click[0] == 1 and not rolled and at_target_square:
+        if click[0] == 1 and not rolled and (at_target_square or not can_move):
+            can_move = True
             current_roll = roll_dice()
             rolled = True
             at_target_square = False
@@ -79,12 +126,35 @@ def roll_button():
         elif click[0] == 0 and rolled and not at_target_square:
             rolled = False
 
+def see_clues():
+    button_width = 80
+    button_height = 80
+    button_x = 20
+    button_y = screen_height - button_height - 20
+
+    button_image = pygame.image.load("assets/clue button.png")
+    button_image = pygame.transform.scale(button_image, (button_width, button_height))
+    screen.blit(button_image, (button_x, button_y))
+
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if button_x + button_width > mouse[0] > button_x and button_y + button_height > mouse[1] > button_y:
+        if click[0] == 1:
+            clues_file = "known clues.txt"
+            with open(clues_file, "r") as file:
+                clues = file.read()
+            pop_up_message(clues)
+
 def main():
     global current_square
     global at_target_square
 
     target_forward_square = 1
     target_reverse_square = 1
+    clue = 1
+
+    clear_text_file("known clues.txt")
  
     while True:
         if rolled:
@@ -103,11 +173,15 @@ def main():
                     current_square = min(len(locations), current_square + 1)  # Ensure current_square doesn't exceed the number of squares
                 elif target_forward_square == current_square or target_reverse_square == current_square:
                     at_target_square = True
+                    pop_up_message(get_clue_from_file("assets/clues/generic/test" + str(clue) + ".txt"))
+                    add_new_clue(get_clue_from_file("assets/clues/generic/test" + str(clue) + ".txt"))
+                    clue += 1
  
         screen.blit(background_image, [0, 0])
 
         draw_circle(current_square)
         roll_button()
+        see_clues()
 
         pygame.display.flip()
  
